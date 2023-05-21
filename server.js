722885c92cc4d82f065a967f9fa9365bdee7fa6f
aungmyooo <aungmo@gmail.com>
2023-05-21 16:33:30 +0630

    update

--- server.js
+++ server.js
@@ -5,38 +5,17 @@ dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
-//console.log(app.get('env'))
-//console.log(process.env);
-
+mongoose.set('strictQuery', false);
 mongoose
   .connect(process.env.CONN_STR, {
     useNewUrlParser: true,
   })
   .then((conn) => {
-    //console.log(conn);
     console.log("DB Conneciton Successful");
   })
   .catch((error) => {
     console.log("Some error has occured");
   });
-
-// testing
-// const testMovie = new Movie({
-//   name: "Die Hard",
-//   description:
-//     "Action packed movie staring bruce willis in this trilling adventure.",
-//   duration: 139,
-//   ratings: 4.5,
-// });
-
-// testMovie
-//   .save()
-//   .then((doc) => {
-//     console.log(doc);
-//   })
-//   .catch((err) => {
-//     console.log("Error occured: " + err);
-//   });
 
 //Create A Server
 const port = process.env.PORT || 3000;
